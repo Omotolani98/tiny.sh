2f32267ef376f177ef1cbc6a937ded144a76f7f8
emmanuel-olat <151914436+emmanuel-olat@users.noreply.github.com>
2025-05-25 13:16:38 +0100

    tui plus deployment to argocd

---
+++ cmd/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	"errors"
+//	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/Omotolani98/tiny.sh/internal/tui"
+//	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/log"
+	"github.com/charmbracelet/ssh"
+	"github.com/charmbracelet/wish"
+	"github.com/charmbracelet/wish/activeterm"
+	"github.com/charmbracelet/wish/bubbletea"
+	"github.com/charmbracelet/wish/logging"
+)
+
+const (
+	host = "0.0.0.0"
+	port = "42000"
+)
+
+func main() {
+	srv, err := wish.NewServer(
+		wish.WithAddress(net.JoinHostPort(host, port)),
+		wish.WithHostKeyPath(".ssh/id_ed25519"),
+		wish.WithMiddleware(
+			bubbletea.Middleware(tui.TeaHandler),
+			activeterm.Middleware(),
+			logging.Middleware(),
+		),
+	)
+
+	if err != nil {
+		log.Error("Could not start server", "error", err)
+	}
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	log.Info("Starting SSH server", "host", host, "port", port)
+
+	go func() {
+		if err = srv.ListenAndServe(); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
+			log.Error("Could not start server", "error", err)
+			done <- nil
+		}
+	}()
+
+	<-done
+	log.Info("Stopping ssh server..")
+	ctx, cancel := context.WithTimeout(context.Background(), 30 * time.Second)
+	defer func ()  { cancel() }()
+	if err := srv.Shutdown(ctx); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
+		log.Error("Could not stop server", "error", err)
+	}
+	/*m := tui.NewModel()
+
+	p := tea.NewProgram(m, tea.WithAltScreen())
+	if _, err := p.Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error running program: %v\n", err)
+    os.Exit(1)
+	}*/
+}
---
+++ helm/templates/_helpers.tpl
@@ -0,0 +1,7 @@
+{{- define "tiny-sh.name" -}}
+tiny-sh
+{{- end }}
+
+{{- define "tiny-sh.fullname" -}}
+{{ .Release.Name }}
+{{- end }}
